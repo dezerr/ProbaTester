0d8062118618dfe125d8c53a3b8c4f34e092a41f
dezerr <63457072+dezerr@users.noreply.github.com>
2021-04-07 00:06:45 +0200

    runner was added

---
+++ runner.py
@@ -0,0 +1,13 @@
+import os
+import time
+from datetime import datetime
+
+hour1 = datetime.now()
+
+for i in range(0, 100):
+    os.system("node demo.js")
+
+hour2 = datetime.now()
+
+print(f"\nHeure de début : {hour1.hour}h {hour1.minute}min {hour1.second}sec")
+print(f"Heure de fin : {hour2.hour}h {hour2.minute}min {hour2.second}sec")
